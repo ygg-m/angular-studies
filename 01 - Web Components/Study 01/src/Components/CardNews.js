74dc181db97410abc8c34d4c898311745407e71d
Ygor Goulart <ygorgm95@gmail.com>
2022-11-08 21:48:31 -0300

    JavaScript component funcional

--- 01 - Web Components/Study 01/src/Components/CardNews.js
+++ 01 - Web Components/Study 01/src/Components/CardNews.js
@@ -16,14 +16,33 @@ class CardNews extends HTMLElement {
     // card left
     const cardLeft = document.createElement("div");
     cardLeft.setAttribute("class", "card__left");
+    // author
     const author = document.createElement("span");
+    const authorName = document.createTextNode(this.getAttribute("authorName"));
     const authorImg = document.createElement("img");
-    authorImg.setAttribute("src", "./assets/profile_ygor.jpeg");
+    authorImg.setAttribute("src", this.getAttribute("authorImg"));
+    author.appendChild(authorImg);
+    author.appendChild(authorName);
+    // title
+    const title = document.createElement("h2");
+    const description = document.createElement("h3");
+    title.innerText = this.getAttribute("title");
+    description.innerText = this.getAttribute("description");
+    // push to card left container
+    cardLeft.appendChild(author);
+    cardLeft.appendChild(title);
+    cardLeft.appendChild(description);
 
     // card right
     const cardRight = document.createElement("div");
     cardRight.setAttribute("class", "card__right");
+    const newsImg = document.createElement("img");
+    newsImg.setAttribute("src", this.getAttribute("newsImg"));
+    cardRight.appendChild(newsImg);
 
+    // push elements to card
+    componentRoot.appendChild(cardLeft);
+    componentRoot.appendChild(cardRight);
     return componentRoot;
   }
 
