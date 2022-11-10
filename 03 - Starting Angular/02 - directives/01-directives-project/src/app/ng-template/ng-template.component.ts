6e5fce0e212a0c610b9a75e27b4730b8c00f0886
Ygor Goulart <ygorgm95@gmail.com>
2022-11-10 20:59:58 -0300

    nh template

--- 03 - Starting Angular/02 - directives/01-directives-project/src/app/ng-template/ng-template.component.ts
+++ 03 - Starting Angular/02 - directives/01-directives-project/src/app/ng-template/ng-template.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ng-template',
   templateUrl: './ng-template.component.html',
-  styleUrls: ['./ng-template.component.sass']
+  styleUrls: ['./ng-template.component.sass'],
 })
 export class NgTemplateComponent implements OnInit {
+  answer1: number = 12;
+  answer1Correct: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onKey(event: any, question: number) {
+    if (question === 1) {
+      if (event.target.value === this.answer1) {
+        this.answer1Correct = true;
+      }
+    }
   }
 
+  constructor() {}
+  ngOnInit(): void {}
 }
