f122f2405f5c3b6ca896967610e5d080ca0c5819
Ygor Goulart <ygorgm95@gmail.com>
2022-11-10 18:30:47 -0300

    folder name and basic structure

--- 03 - Starting Angular/life-cycle/src/app/app.module.ts
+++ 03 - Starting Angular/life-cycle/src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TitleComponent } from './Components/title/title.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, TitleComponent],
+	imports: [BrowserModule, AppRoutingModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
