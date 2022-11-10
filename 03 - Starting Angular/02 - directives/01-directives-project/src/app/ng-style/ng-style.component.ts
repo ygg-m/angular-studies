ed6ecb0a0a354393c900df8923916971be189ad2
Ygor Goulart <ygorgm95@gmail.com>
2022-11-10 19:50:27 -0300

    using ngStyle

--- 03 - Starting Angular/02 - directives/01-directives-project/src/app/ng-style/ng-style.component.ts
+++ 03 - Starting Angular/02 - directives/01-directives-project/src/app/ng-style/ng-style.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ng-style',
   templateUrl: './ng-style.component.html',
-  styleUrls: ['./ng-style.component.sass']
+  styleUrls: ['./ng-style.component.sass'],
 })
 export class NgStyleComponent implements OnInit {
+  bgColor: string = '';
+  txtColor: string = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  changeBackground(value: string) {
+    this.bgColor = value;
   }
 
+  changeText(value: string) {
+    this.txtColor = value;
+  }
+  constructor() {}
+
+  ngOnInit(): void {}
 }
